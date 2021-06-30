d3b883f1ebedcef63128369339726efb27816d18
Norulshahlam <norulshahlam@gmail.com>
2021-06-30 21:43:12 +0800

    3

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-	@GetMapping(path = "")
+	@GetMapping(path = "/")
 	public String helloWorld() {
 		return "Hello this is the index page";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello World");
+		return new HelloWorldBean("Hello");
 	}
 
 	/// hello-world/path-variable/in28minutes
